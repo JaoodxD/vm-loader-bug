257cfd0a03a13c1556c8aa650c34ce7ad0915c47
JaoodxD <jaood96@gmail.com>
2023-03-24 13:33:44 +0200

    update load to match original one

--- load.js
+++ load.js
@@ -15,8 +15,7 @@ const load = async (filePath, sandbox) => {
   const script = new vm.Script(code);
   const context = vm.createContext(Object.freeze({ ...sandbox }));
   const exported = script.runInContext(context, OPTIONS);
-  // return typeof exported === 'object' ? exported : { method: exported };
-  return exported;
+  return typeof exported === 'object' ? exported : { method: exported };
 };
 
 const loadDir = async (folderPath, sandbox) => {
